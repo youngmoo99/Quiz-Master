8624d49db176b4414d5ea687b33b2e44ff384617
YoungMoo Kim <rladuan612@gmail.com>
2025-03-17 19:38:09 +0900

    update question

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")] //스크립터블 오브젝트를 사용하기 위해 속성 추가
+public class QuestionSO : ScriptableObject // -->스크립터블 오브젝트 
+{   
+    [TextArea(2,6)] // 2줄 최소 6줄 최대
+    [SerializeField] string question = "Which of the following is not a Hoyoverse game"; 
+
+    public string GetQuestion() 
+    {
+
+
+        return question;
+    }
+
+}
+
